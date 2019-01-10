5070e2f45e240e0df6bbe292357997d31e03ea88
wolfpan <haoyupan@aliyun.com>
2019-01-10 10:59:21 +0800

    Delete main.py

--- test/main.py
+++
@@ -1,9 +0,0 @@
-import config
-import core
-
-for remote, local in config.STORAGE.items():
-    repo = config.github.get_repo('panhaoyu/' + remote)
-    storage = core.Storage(local, repo)
-    storage.upload()
-
-input('Press Enter to Exit.')
